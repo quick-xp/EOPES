42e2902314e9120b6784e41c2e3e37820095bbd4
rnishio <quick.xp@me.com>
2015-03-15 13:44:58 +0900

    estimate show pageでregionName solarSystemName を表示するように修正

--- app/helpers/estimates_helper.rb
+++ app/helpers/estimates_helper.rb
@@ -1,2 +1,19 @@
 module EstimatesHelper
+  def get_region_name(region_id)
+    region = MapRegion.where(:regionID => region_id).first
+    if region.nil?
+      "All Region"
+    else
+      region.regionName
+    end
+  end
+
+  def get_solar_system_name(solar_system_id)
+    solar_system = MapSolarSystem.where(:solarSystemID => solar_system_id).first
+    if solar_system.nil?
+      "All Solar System"
+    else
+      solar_system.solarSystemName
+    end
+  end
 end
