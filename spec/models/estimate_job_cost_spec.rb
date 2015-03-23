df46dfdef4e527bd4901c6f900d2deb3471574d5
rnishio <quick.xp@me.com>
2015-03-24 00:08:32 +0900

    estimate_job_cost_spec テストケース記載

--- spec/models/estimate_job_cost_spec.rb
+++ spec/models/estimate_job_cost_spec.rb
@@ -18,5 +18,34 @@
 require 'rails_helper'
 
 RSpec.describe EstimateJobCost, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "System Cost Index の取得" do
+
+    context "Region Id と Solar System Id 両方指定されている かつ 存在するIDである場合" do
+      it "SolarSystemのCost_Indexを返す"
+    end
+
+    context "Region Id が指定されているが Solar System Id が 指定されていない(nil) の場合" do
+      it "Region内のCost_Indexの平均を返す"
+    end
+
+    context "Region Id と Solar System Id 両方指定されていない場合" do
+      it "Universe全体のCost_Indexの平均を返す"
+    end
+
+    context "Region Id が指定されているが 存在しない Region Id の場合" do
+      it "0を返す"
+    end
+
+    context "Solar System Id が指定されているが 存在しない Solar System Id の場合" do
+      it "0を返す"
+    end
+  end
+
+  describe "Job Cost 再計算" do
+      it "Base Job Cost は Σ(baseQuantity * adjustedPrice) * Runs"
+      it "JobFee は system_cost_index * baseJobCost"
+      it "FacilityCost は JobFee * taxRate / 100"
+      it "Total Job Cost は JobFee + FacilityCost"
+  end
 end
+
