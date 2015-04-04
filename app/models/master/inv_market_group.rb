c3c993083455f3b173daad0e481e8c057c1bc8f1
rnishio <quick.xp@me.com>
2015-04-04 16:24:58 +0900

    inv_market_group bundle exec annotate

--- app/models/master/inv_market_group.rb
+++ app/models/master/inv_market_group.rb
@@ -1,20 +1,13 @@
 # == Schema Information
 #
-# Table name: invTypes
+# Table name: invMarketGroups
 #
-#  typeID              :integer          not null, primary key
-#  groupID             :integer
-#  typeName            :string(100)
-#  description         :string(3000)
-#  mass                :float(53)
-#  volume              :float(53)
-#  capacity            :float(53)
-#  portionSize         :integer
-#  raceID              :integer
-#  basePrice           :decimal(19, 4)
-#  published           :integer
-#  marketGroupID       :integer
-#  chanceOfDuplicating :float(53)
+#  marketGroupID   :integer          not null, primary key
+#  parentGroupID   :integer
+#  marketGroupName :string(100)
+#  description     :string(3000)
+#  iconID          :integer
+#  hasTypes        :boolean
 #
 
 class InvMarketGroup < ActiveRecord::Base
