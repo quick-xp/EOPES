f41e8be7f5e1d84fd8cf163c312047bca02bcc7b
rnishio <quick.xp@me.com>
2015-04-04 14:45:51 +0900

    bundle exec annotate

--- app/controllers/market_explorer_controller.rb
+++ app/controllers/market_explorer_controller.rb
@@ -1,4 +1,8 @@
 class MarketExplorerController < ApplicationController
   def index
   end
+
+  def get_item_list
+
+  end
 end
--- app/models/master/inv_market_group.rb
+++ app/models/master/inv_market_group.rb
@@ -12,29 +12,14 @@
 #  portionSize         :integer
 #  raceID              :integer
 #  basePrice           :decimal(19, 4)
-#  published           :boolean
+#  published           :integer
 #  marketGroupID       :integer
 #  chanceOfDuplicating :float(53)
 #
 
 class InvType < ActiveRecord::Base
-  self.table_name = 'invTypes'
-  self.primary_key = 'typeID'
+  self.table_name = 'invMarketGroups'
+  self.primary_key = 'marketGroupID'
   establish_connection(:EveMasterDB)
 
-  def self.get_type_name(typeID)
-    InvType.find(typeID).try(:typeName)
-  end
-
-  def self.get_type_volume(typeID)
-    InvType.find(typeID).try(:volume)
-  end
-
-  def get_typeName_jp
-    TrnTranslation.where(['keyID = ? and languageID = ? and tcID = ?', self.typeID, 'JA', 8]).first.try(:text)
-  end
-
-  def get_invType_description_jp
-    TrnTranslation.where(['keyID = ? and languageID = ? and tcID = ?', self.typeID, 'JA', 33]).first.try(:text)
-  end
 end
